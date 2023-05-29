cf5807a366c8a777dcd871706d479c9850cacfd2
박세진 <jinpark99@naver.com>
2023-05-29 17:51:11 +0900

    Init Commit

---
+++ src/main/java/study/querydsl/QueryDslPracticeApplication.java
@@ -0,0 +1,13 @@
+package study.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryDslPracticeApplication.class, args);
+	}
+
+}
